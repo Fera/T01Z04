a9086ecfe18210fa9e5f79af70851a927c9bc838
Joanna Bieńkowska <ferart.pl@gmail.com>
2017-03-10 01:10:48 +0100

    Poprawki w skrypcie po konsultacji

--- js/scripts.js
+++ js/scripts.js
@@ -9,30 +9,21 @@
         var form = $("#name-form"),
             field = $(form).find("input#name"), // zmienna zwracająca input name 
             button = $("button.add-btn");
+             
 
         $(button).on("click", function(){ 
 
             // wyłączenie natywnej funkcji buttona - zastąpiłam je przez type="button" w index.html
-                                               
             
-            if( $.trim(field.val()) === "" ) { // metoda .trim usuwa białe znaki z przodu i z tyłu, sprawdzenie czy pole input id="name" jest puste
-                
-            // jeśli jest - nic się nie dzieje
-            }
-
-            else {
-                
-                // jeśli nie jest - zostaje wstawiona jego zawartość do ul jako kolejny punkt na liście
-                
+            if( $.trim(field.val()) !== "" ) { // metoda .trim usuwa białe znaki z przodu i z tyłu  
+                           
                 //zmienna zwracająca element listy z zawartością treści wpisanej w pole input
                 var listElement = $("<li></li>", {
-
                     text: $(field).val()
-
-                });                           
+                });                         
+                                     
                 
-                // $("ul.name-list").append(listElement); // utworzenie listy
-                
+                // $("ul.name-list").append(listElement); // utworzenie listy                
                 listElement.fadeIn(500).appendTo(".name-list");
                 
             }
