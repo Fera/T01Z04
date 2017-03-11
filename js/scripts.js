950fef7729a33ca2ebc3f023c3a03229365c0d89
Joanna Bieńkowska <ferart.pl@gmail.com>
2017-03-11 12:38:11 +0100

    optymalizacja kodu

--- js/scripts.js
+++ js/scripts.js
@@ -12,20 +12,22 @@
 
         $(button).on("click", function(){ 
 
+            var val = field.val();
+
             // wyłączenie natywnej funkcji buttona - zastąpiłam je przez type="button" w index.html
             
-            if( $.trim(field.val()) !== "" ) { // metoda .trim usuwa białe znaki z przodu i z tyłu  
+            if( $.trim(val) !== "" ) { // metoda .trim usuwa białe znaki z przodu i z tyłu  
                            
                 //zmienna zwracająca element listy z zawartością treści wpisanej w pole input
                 var listElement = $("<li></li>", {
-                    text: $(field).val()
+                    text: val
                 });                 
                                      
                 // $("ul.name-list").append(listElement); // utworzenie listy                
                 listElement.fadeIn(500).appendTo(".name-list");                
             }
 
-            $(field).val(''); //czyści input po kliknięciu             
+            field.val(''); //czyści input po kliknięciu             
         }); 
     });
 })(jQuery, window, document);
